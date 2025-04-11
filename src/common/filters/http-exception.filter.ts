618bfbbf727b8d5ff5a88da4ec27ff56ca6d6cec
en0ku <en0ku@qwe.me>
2025-04-11 10:01:14 +0300

    upd error structure

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -9,31 +9,39 @@ import { Response } from 'express';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost): void {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    const httpStatus =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    if (exception instanceof HttpException) {
+      const httpStatus = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
 
-    const exceptionResponse = exception.getResponse();
-    const message =
-      typeof exceptionResponse === 'object' &&
-      'message' in exceptionResponse &&
-      Array.isArray(exceptionResponse.message)
-        ? Object.values(exceptionResponse.message).join(',\n')
-        : exceptionResponse;
+      const message =
+        typeof exceptionResponse === 'object' &&
+        'message' in exceptionResponse &&
+        Array.isArray(exceptionResponse.message)
+          ? Object.values(exceptionResponse.message).join(',\n')
+          : exception.message;
 
-    const responseBody = {
+      const responseBody = {
+        success: false,
+        result: {
+          status: httpStatus,
+          error: message,
+        },
+      };
+
+      response.status(httpStatus).json(responseBody);
+      return;
+    }
+
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       success: false,
       result: {
-        status: httpStatus,
-        message,
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'Internal server Error',
       },
-    };
-
-    response.status(httpStatus).json(responseBody);
+    });
   }
 }
